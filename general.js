6a4a64e5a3c35b1501e364ae3ecff6d1ee6153f3
tridipdowari <tridipdowari75@gmail.com>
2026-05-08 20:52:55 +0530

    updated general.js

--- general.js
+++ general.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-// Get all books
+// Task 10: Get all books
 axios.get('http://localhost:3000/')
 .then(response => {
     console.log("All Books:");
@@ -10,7 +10,7 @@ axios.get('http://localhost:3000/')
     console.log(error);
 });
 
-// Get book by ISBN
+// Task 11: Get book by ISBN using Promises
 axios.get('http://localhost:3000/isbn/1')
 .then(response => {
     console.log("Book by ISBN:");
@@ -20,22 +20,28 @@ axios.get('http://localhost:3000/isbn/1')
     console.log(error);
 });
 
-// Get books by Author
-axios.get('http://localhost:3000/author/Author1')
-.then(response => {
-    console.log("Books by Author:");
-    console.log(response.data);
-})
-.catch(error => {
-    console.log(error);
-});
+// Task 12: Get books by Author using async/await
+async function getByAuthor() {
+    try {
+        const response = await axios.get('http://localhost:3000/author/Author1');
+        console.log("Books by Author:");
+        console.log(response.data);
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-// Get books by Title
-axios.get('http://localhost:3000/title/Book1')
-.then(response => {
-    console.log("Books by Title:");
-    console.log(response.data);
-})
-.catch(error => {
-    console.log(error);
-});
+getByAuthor();
+
+// Task 13: Get books by Title using async/await
+async function getByTitle() {
+    try {
+        const response = await axios.get('http://localhost:3000/title/Book1');
+        console.log("Books by Title:");
+        console.log(response.data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getByTitle();
